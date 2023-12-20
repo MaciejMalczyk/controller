d95f5d517ee42a54ad72beb491415226fe3cab4b
Maciej Malczyk <wujekbrezniew@gmail.com>
2023-12-20 12:38:11 +0100

    Add systemd service file

--- controller-rust/src/config.rs
+++ controller-rust/src/config.rs
@@ -2,7 +2,7 @@ use std::fs;
 use serde_json;
 
 pub fn read() -> serde_json::Value {
-    let file = fs::File::open("src/config.json").unwrap();
+    let file = fs::File::open("config.json").unwrap();
     let json: serde_json::Value = serde_json::from_reader(file).unwrap();
     return json;
 }
