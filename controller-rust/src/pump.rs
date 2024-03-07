392c112d0e16a0b13ea600fbb4317829939e14de
Maciej Malczyk <wujekbrezniew@gmail.com>
2024-03-07 14:35:47 +0100

    Add watchdog for pump

--- controller-rust/src/pump.rs
+++ controller-rust/src/pump.rs
@@ -9,6 +9,7 @@ pub struct Pump {
     enable: Arc<Mutex<bool>>,
     moisture: Arc<Mutex<f64>>,
     from_interface: Arc<Mutex<f64>>,
+    from_cultivation: Arc<Mutex<bool>>,
 }
 
 impl Pump {
@@ -18,12 +19,14 @@ impl Pump {
             enable: Arc::new(Mutex::new(false)),
             moisture: Arc::new(Mutex::new(0.0)),
             from_interface: Arc::new(Mutex::new(0.0)),
+            from_cultivation: Arc::new(Mutex::new(true)),
         };
         p
     }
     
     pub async fn set_moisture(&mut self, moisture: f64) {
         *self.moisture.lock().await = moisture;
+        *self.from_cultivation.lock().await = true;
     }
     
     pub async fn set_from_interface(&mut self, val: f64) {
@@ -43,33 +46,51 @@ impl Pump {
     }
     
     pub async fn start(&mut self) {
+        println!("PUMP ON");
         *self.enable.lock().await = true;
         
         tokio::spawn({
-            let pin_clone = Arc::clone(&self.pin);
-            let fi = Arc::clone(&self.from_interface);
-            let m = Arc::clone(&self.moisture);
             let enable_clone = Arc::clone(&self.enable);
+            let pin_clone = Arc::clone(&self.pin);
+            let fi_clone = Arc::clone(&self.from_interface);
+            let moisture_clone = Arc::clone(&self.moisture);
+            let fc_clone = Arc::clone(&self.from_cultivation);
             async move {
-                let mut old_moisture = 0.0;
-                loop {
-                    if *enable_clone.lock().await == false {
-                        println!("PUMP OFF");
-                        break;
+                let watchdog = async {
+                    loop {
+                        *fc_clone.lock().await = false;
+                        sleep(Duration::from_secs(150)).await;
+                        if *fc_clone.lock().await == false {
+                            println!("SENSOR FAILURE");
+                            *enable_clone.lock().await = false;
+                            break;
+                        }
                     }
-                    if old_moisture == *fi.lock().await {
-                        println!("MOISTURE SENSOR ERROR");
-                        break;
+                };
+                
+                let process = async {
+                    loop {
+                        if *enable_clone.lock().await == false {
+                            println!("PUMP OFF");
+                            break;
+                        }
+                        if *moisture_clone.lock().await >= 0.0 && *fi_clone.lock().await > *moisture_clone.lock().await {
+                            pin_clone.lock().await.set(255).unwrap();
+                            println!("PUMP PUSH");
+                            sleep(Duration::from_secs(2)).await;
+                            pin_clone.lock().await.set(0).unwrap();
+                            println!("PUMP STOP");
+                        }
+                        sleep(Duration::from_secs(300)).await;
                     }
-                    if *m.lock().await >= 0.0 && *fi.lock().await > *m.lock().await {
-                        pin_clone.lock().await.set(255).unwrap();
-                        println!("PUMP ON");
-                        sleep(Duration::from_secs(2)).await;
-                        old_moisture = *fi.lock().await;
-                        pin_clone.lock().await.set(0).unwrap();
-                    }
-                    sleep(Duration::from_secs(120)).await;
-                }
+                    
+                };
+                
+                tokio::join!(
+                    process,
+                    watchdog,
+                );
+                
             }
         });
     }
